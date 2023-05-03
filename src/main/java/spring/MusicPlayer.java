e944e19589bafe6d5120cd2773a5e3414c0e2065
mishanya2k <103316910+mishanya2k@users.noreply.github.com>
2023-05-03 15:52:10 +1000

    musicList added, changed method playMusic added musicPlayer.properties file DI introducing with setter

--- src/main/java/spring/MusicPlayer.java
+++ src/main/java/spring/MusicPlayer.java
@@ -1,13 +1,41 @@
 package spring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private IMusic music;
 
-    public MusicPlayer(IMusic music) {
-        this.music = music;
+
+    private List<IMusic> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public void setMusicList(List<IMusic> musicList) {
+        this.musicList = musicList;
     }
 
-    public void playMusic(){
-        System.out.println("Playing: " + music.getSong() + " by " + music.getAuthor());
+    public void playMusic() {
+        for (IMusic music : musicList) {
+            System.out.println("Playing " + music.getSong() + " by " + music.getAuthor());
+        }
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+
 }
+
