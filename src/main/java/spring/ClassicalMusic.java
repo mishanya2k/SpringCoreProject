83fb605e0e89f519afc28f73da4519cf0e3c7111
mishanya2k <103316910+mishanya2k@users.noreply.github.com>
2023-05-03 17:19:12 +1000

    added init/destroy/factory method

--- src/main/java/spring/ClassicalMusic.java
+++ src/main/java/spring/ClassicalMusic.java
@@ -1,10 +1,24 @@
 package spring;
 
 public class ClassicalMusic implements IMusic{
+    private ClassicalMusic(){}
+
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+    public void doMyInit(){
+        System.out.println("doing my initialization");
+    }
+
+    public void doMyDestroy(){
+        System.out.println("doing my destroying");
+    }
+    @Override
     public String getSong() {
         return "Moonlight Sonata";
     }
 
+    @Override
     public String getAuthor() {
         return "Ludwig van Beethoven";
     }
--- src/main/java/spring/TestSpring.java
+++ src/main/java/spring/TestSpring.java
@@ -5,11 +5,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
-
+        ClassicalMusic classicalMusic = context.getBean("classicalMusic", ClassicalMusic.class);
+        context.close();
     }
 }
 
